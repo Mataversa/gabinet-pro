ddb88ba4cc64e1e1d826b46b4004cc1a038283e6
Mataversa <vorezny@gmail.com>
2025-03-23 23:33:04 +0100

    Nowy system autoryzacji

--- auth.js
+++ auth.js
@@ -22,3 +22,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+function checkPassword() {
+  const password = document.getElementById("password").value;
+  const errorMessage = document.getElementById("errorMessage");
+
+  if (password === "gabinet") {
+    localStorage.setItem("isAuthenticated", "true");
+    window.location.href = "index.html";
+  } else {
+    errorMessage.textContent = "Nieprawidłowe hasło. Spróbuj ponownie.";
+    document.getElementById("password").value = "";
+  }
+}
